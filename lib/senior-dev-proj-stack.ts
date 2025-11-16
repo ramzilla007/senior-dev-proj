92ab873681a3b14d1015816e1104676fd8e37dab
Rami Tarazi <tarazi@yahoo.com>
2025-11-16 14:46:39 -0800

    Code updates

--- lib/senior-dev-proj-stack.ts
+++ lib/senior-dev-proj-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib/core';
+import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Table, AttributeType } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { CfnIntegrationAssociation } from 'aws-cdk-lib/aws-connect';
+import { join } from 'path';
 
 export class SeniorDevProjStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // DynamoDB table
+    const table = new Table(this, 'VanityNumberTable', {
+      partitionKey: { name: 'id', type: AttributeType.STRING },
+      tableName: 'VanityNumberTable',
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SeniorDevProjQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Lambda function
+    const lambdaFunction = new NodejsFunction(this, 'LambdaFunction', {
+      runtime: Runtime.NODEJS_20_X,
+      entry: join(__dirname, '../Services/handler.ts'),
+      handler: 'handler',
+      environment: {
+        TABLE_NAME: table.tableName
+      }
+    });
+
+    // Give Lambda permission to write to table
+    table.grantWriteData(lambdaFunction);
   }
 }
